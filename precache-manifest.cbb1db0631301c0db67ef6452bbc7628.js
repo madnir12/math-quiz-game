30ee366f1fec6214b4d6c88d288817238aa84d21
madnir12 <anfz12001@gmail.com>
2022-11-28 06:40:02 +0500

    Updates

---
+++ precache-manifest.cbb1db0631301c0db67ef6452bbc7628.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a019fd24b86c24046565cda18b1a9b1",
+    "url": "/math-quiz-game/index.html"
+  },
+  {
+    "revision": "19670fd9a7b0d47a93fb",
+    "url": "/math-quiz-game/static/css/main.dc54b24a.chunk.css"
+  },
+  {
+    "revision": "30b54398463f6c1d06a6",
+    "url": "/math-quiz-game/static/js/2.301b1066.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/math-quiz-game/static/js/2.301b1066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19670fd9a7b0d47a93fb",
+    "url": "/math-quiz-game/static/js/main.c059ef59.chunk.js"
+  },
+  {
+    "revision": "a863bd95c961deb433de",
+    "url": "/math-quiz-game/static/js/runtime-main.bfd83d66.js"
+  },
+  {
+    "revision": "093eed4f4091d0792b60b33a1d98448c",
+    "url": "/math-quiz-game/static/media/Wrong-answer-sound-effect.093eed4f.mp3"
+  },
+  {
+    "revision": "d0c5f2229fb79adc465123e0629687bb",
+    "url": "/math-quiz-game/static/media/correct-answer.d0c5f222.mp3"
+  }
+]);
